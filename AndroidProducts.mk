08ad34dafc9aa739c06ae0c08b45c14068d65538
aniket-earth <aniket.earth@gmail.com>
2026-06-02 15:31:54 +0000

    asteroids: Init Matrixx
    Signed-off-by: aniket-earth <aniket.earth@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,4 +4,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_asteroids.mk
+    $(LOCAL_DIR)/matrixx_asteroids.mk
--- matrixx_asteroids.mk
+++ matrixx_asteroids.mk
@@ -7,15 +7,29 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 $(call inherit-product, device/nothing/asteroids/device.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit some common Matrixx stuff.
+$(call inherit-product, vendor/matrixx/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Nothing
 PRODUCT_DEVICE := asteroids
 PRODUCT_MANUFACTURER := Nothing
 PRODUCT_MODEL := A059
-PRODUCT_NAME := lineage_asteroids
+PRODUCT_NAME := matrixx_asteroids
 
 PRODUCT_GMS_CLIENTID_BASE := android-nothing
+
+#Inherit Matrix Flags
+MATRIXX_MAINTAINER := Aniket
+TARGET_INCLUDE_MATLOG := false
+TARGET_CUSTOM_UDFPS := true
+WITH_GMS := true
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+WITH_BCR := false
+TARGET_OPTIMIZED_DEXOPT := true
+USE_REALITY_ENGINE := true
+BYPASS_CHARGE_SUPPORTED := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="qssi_64-user 16 BQ2A.250721.001-BP2A.250605.031.A3 2604141749 release-keys" \
